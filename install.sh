30ab4ae4126ae1e49c004ec38aab0568e704d5e7
William Rigaut <rigautwilliam@gmail.com>
2024-04-26 11:08:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 pip install ipywidgets matplotlib tqdm nodejs npm fabio
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
-python3.10 -m ipykernel install --user
 mkdir data results
 mkdir data/XRD results/XRD
+#python3.10 -m ipykernel install --user
