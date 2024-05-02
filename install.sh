223eea7d4bd08b2444d229a54f95d822db31e9da
William Rigaut <rigautwilliam@gmail.com>
2024-05-02 16:28:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-pip install ipywidgets matplotlib tqdm nodejs npm fabio openpyxl
+pip install ipywidgets matplotlib tqdm nodejs npm fabio openpyxl numba
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
 mkdir data results
 mkdir data/XRD results/XRD data/MOKE results/MOKE data/EDX results/EDX
-#python3.10 -m ipykernel install --user
+
+#use this if you're using multiple versions of python on your computer (remplace 3.X by the version of python you installed)
+#python3.X -m ipykernel install --user
